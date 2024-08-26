4f3bb74d6b66a942162dd9db56cef2ee5f6ddf1d
Antonio Magalhaes <anm@asg-digital.dk>
2024-08-26 09:21:36 +0200

    re-release? maybe

---
+++ src/app/component-lib/components/index.js
@@ -0,0 +1,8 @@
+// src/components/index.ts
+
+export * from './inbox-screen.component';
+export * from './pure-inbox-screen.component';
+export * from './pure-inbox-screen.stories';
+export * from './pure-task-list.component';
+export * from './task-list.component';
+export * from './task.module';
